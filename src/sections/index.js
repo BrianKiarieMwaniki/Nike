6dc3dac8de66e98c185f2e78b4af329ac12c7b69
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-11-15 12:52:15 +0300

    scaffold sections and components

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SupperQuality from "./SupperQuality";
+import PopularProducts from "./PopularProducts";
+
+export {
+  CustomerReviews,
+  Footer,
+  Hero,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SupperQuality,
+  PopularProducts,
+};
